354c25e770ec5996b2325ea4868dd3d2fe7c00e6
jon <jonrose08@gmail.com>
2012-09-08 18:32:26 -0700

    init commit

--- app.rb
+++ app.rb
@@ -9,6 +9,55 @@ configure do
   enable :sessions
 end
 
+def partial(template,locals=nil)
+  if template.is_a?(String) || template.is_a?(Symbol)
+    template=(template.to_s).to_sym
+  else
+    locals=template
+    template=template.is_a?(Array) ? (template.first.class.to_s.downcase).to_sym : (template.class.to_s.downcase).to_sym
+  end
+  if locals.is_a?(Hash)
+    haml(template,{:layout => false},locals)      
+  elsif locals
+    locals=[locals] unless locals.respond_to?(:inject)
+    locals.inject([]) do |output,element|
+      output <<     erb(template,{:layout=>false},{template.to_s.delete("_").to_sym => element})
+    end.join("\n")
+  else 
+    haml(template,{:layout => false})
+  end
+end
+
+def time_info
+  {title: "Time",
+   input_fields: [{name: "start-date", type: :date, placeholder: "start-date"},
+                  {name: "start", type: :text, placeholder: "starting (12:00:00)"},
+                  {name: "end-date", type: :date, placeholder: "end-date"},
+                  {name: "end", type: :text, placeholder: "ending (12:00:00)"}]}
+end
+
+def location_info
+  {title: "Location",
+   input_fields: [{name: "city", type: :text, placeholder: "city"},
+                  {name: "country", type: :text, placeholder: "country"},
+                  {name: "country-code", type: :text, placeholder: "country code"}]}
+end
+
+def event_info
+  {title: "Event Info",
+   input_fields: [{name: "title", type: :text, placeholder: "title"},
+                  {name: "description", type: :text, placeholder: "description"}]}
+end
+
+def event_form
+  {title: "Create an Event",
+   elements: [event_info,
+              time_info,
+              location_info],
+   action: "/create-event",
+   method: "post"}
+end
+
 get '/' do
   @user = session[:user]
   unless @user == nil
@@ -18,9 +67,59 @@ get '/' do
 end
 
 get '/user/:user_name/dashboard' do
-  @user = User.first(user_name: params[:user_name])
+  @user = session[:user]
   @user_name = session[:user].user_name
+  @user_events = Event.all(:user => @user)
   haml :user_dashboard
+end
+
+get '/user/:user_name/create-event' do
+  @user = User.first(user_name: params[:user_name])
+  @content = partial(:form, {form_map: event_form})
+  @items = [["Test", "/test/link"],
+            ["Test", "/test/link"],
+            ["Test", "/test/link"],
+            ["Test", "/test/link"],
+            ["Test", "/test/link"]]
+  @sidebar = partial(:sidebar, {items: @items})
+  haml :with_sidebar
+end
+
+post'/user/:user_name/create-event' do
+  @user = User.first(user_name: params[:user_name])
+  event = Event.new
+  event.user = @user
+  event.title = params["title"]
+  event.body = params["body"]
+  event.img_url = params["img_url"]
+  
+  time = Time.new
+  time.event = event
+
+  location = Location.new
+  location.event = event
+
+  if event.save && time.save && location.save
+    flash("Event created")
+    redirect '/user/' << session[:user].user_name.to_s << "/dashboard"
+  else
+    tmp = []
+    event.errors.each do |e|
+      tmp << (e.join("<br/>"))
+    end
+    flash(tmp)
+    redirect '/user/:user_name/create-event'
+  end
+end
+
+
+get '/user/dashboard' do
+  if session[:user] == nil
+    @user_name = session[:user].user_name
+    haml :user_dashboard
+  else
+    haml :no_user_dash
+  end
 end
 
 get '/user' do
@@ -38,7 +137,6 @@ get '/contact' do
   haml :contact
 end
 
-
 get '/user/:user_name/profile' do
   @user = User.first(user_name: params[:user_name])
   unless session[:user] == nil
@@ -46,7 +144,6 @@ get '/user/:user_name/profile' do
   end
   haml :user_profile
 end
-
 
 get '/login' do
   haml :login
@@ -100,4 +197,7 @@ post '/register' do
     redirect '/create'
   end
 end
+
+
+
 
--- model/dm.rb
+++ model/dm.rb
@@ -5,55 +5,151 @@ require 'dm-migrations'
 require './helpers/helpers'
 
 
-DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/user.db")
+DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/base.db")
+
+
+class Profile
+	include DataMapper::Resource
+	property :id,		Serial
+	property :first_name, String
+	property :last_name, String
+	property :age, 		Integer
+	property :gender,	String
+	property :bio,		Text
+	belongs_to :user
+end
 
 class User
+	include DataMapper::Resource
+	property :id,               	Serial
+	property :user_name,        	String, key: true, length: (3..40), required: true
+	property :img_url,          	String
+	property :email,            	String
+	property :password,         	String
+	property :salt,             	String
+	property :hashed_password,  	String
+	property :created_at,       	DateTime, default: DateTime.now
+	property :upadted_at,       	DateTime
+
+	has 1, :profile
+
+	def username= new_username
+		@username = new_username.downcase
+	end
+
+	def password=(pass)
+		@password = pass
+		self.salt = random_string(10) unless self.salt
+		self.hashed_password = User.encrypt(@password, self.salt)
+	end
+
+	def self.encrypt(pass, salt)
+		Digest::SHA1.hexdigest(pass + salt)
+	end
+
+	def self.authenticate(login, pass)
+		u = User.first(user_name: login)
+		return nil if u.nil?
+		return u if User.encrypt(pass, u.salt) == u.hashed_password
+		nil
+	end
+
+	def random_string(len)
+		chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+		str = ""
+		1.upto(len) { |i| str << chars[rand(chars.size-1)] }
+		return str
+	end
+end
+
+class Location
+	include DataMapper::Resource
+	property :id,       	Serial
+	property :city,			String
+	property :country,		String
+	property :geo_location, String
+
+	property :upadted_at, 	DateTime
+	property :created_at, 	DateTime, default: DateTime.now  # A DateTime, for any date you might like.
+
+	belongs_to :event
+end
+
+class Time
+	include DataMapper::Resource
+	property :id,       	Serial
+	property :start,		DateTime
+	property :end,			DateTime
+	property :duration,		String
+	property :created_at, 	DateTime, default: DateTime.now  # A DateTime, for any date you might like.
+end
+
+class Event
+	include DataMapper::Resource
+	## who
+	belongs_to :user
+	## what
+	property :id,         Serial    # An auto-increment integer key
+	property :title,      String    # A varchar type string, for short strings
+	property :permission, String
+	property :upadted_at, DateTime
+	property :created_at, DateTime, default: DateTime.now  # A DateTime, for any date you might like.
+	## Where
+	has n, :locations
+	## when
+	has n, :times
+	## why
+	property :body,       Text      # A text block, for longer string data.
+	property :img_url,    String
+	has n, :comments
+
+	before :update, :update_update_time
+
+	def update_update_time
+		@upadted_at = DateTime.now
+	end
+end 
+
+class Time
+	belongs_to :event
+end
+
+class Comment
   include DataMapper::Resource
 
-  property(:id,               Serial)
+  property :id,         Serial
+  property :posted_by,  String
+  property :email,      String
+  property :url,        String
+  property :body,       Text
+end
 
-  property(:user_name,        String, key: true, length: (3..40), required: true)
+class Category
+  include DataMapper::Resource
 
-  property(:img_url,          String)
+  property :id,         Serial
+  property :name,       String
+end
 
-  property(:email,            String)
+class Categorization
+	include DataMapper::Resource
+	
+	property :id,         	Serial
+	property :created_at, 	DateTime
+	
+	belongs_to :category
+	belongs_to :event
+end
 
-  property(:password,         String)
-  property(:salt,             String)
-  property(:hashed_password,  String)
+# Now we re-open our Event and Categories classes to define associations
+class Event
+  has n, :categorizations
+  has n, :categories, :through => :categorizations
+end
 
-  property(:created_at,       DateTime, default: DateTime.now)
-  property(:upadted_at,       DateTime)
-
-
-  def username= new_username
-    @username = new_username.downcase
-  end
-
-  def password=(pass)
-    @password = pass
-    self.salt = random_string(10) unless self.salt
-    self.hashed_password = User.encrypt(@password, self.salt)
-  end
-
-  def self.encrypt(pass, salt)
-    Digest::SHA1.hexdigest(pass + salt)
-  end
-  
-  def self.authenticate(login, pass)
-    u = User.first(user_name: login)
-    return nil if u.nil?
-    return u if User.encrypt(pass, u.salt) == u.hashed_password
-    nil
-  end
-
-  def random_string(len)
-    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
-    str = ""
-    1.upto(len) { |i| str << chars[rand(chars.size-1)] }
-    return str
-  end
-
+class Category
+  has n, :categorizations
+  has n, :events,      :through => :categorizations
 end
 
 def get_day(record)
@@ -65,3 +161,14 @@ def get_month(creation_date)
 end
 
 DataMapper.auto_upgrade!
+
+
+
+
+
+
+@new_user = User.create(user_name: "test", 
+						email: "test@test-email.com",
+						password: "test")
+
+

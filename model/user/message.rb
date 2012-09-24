e9bd31bc8cb6c3e465d3467ed5aa884d5da05f7e
jon <jonrose08@gmail.com>
2012-09-24 12:44:57 -0700

    add messaging

---
+++ model/user/message.rb
@@ -0,0 +1,37 @@
+require 'dm-core'
+
+class SMessage
+  include DataMapper::Resource
+  property :id, 	Serial
+  property :subject, String, default: ""
+  property :body, 	Text, default: ""
+  property :created_at, DateTime, default: DateTime.now
+  property :star, 	Boolean, default: false
+  property :sent, Boolean, default: false
+  property :target, String
+
+  def send(target_user)
+  	msg = RMessage.create(subject: self.subject, body: self.body, user: target_user, source: self.user.user_name)
+    success = msg.save
+  	if success
+  		self.sent = true
+      puts self.sent
+      self.target = target_user.user_name
+      save
+  		return true
+  	else
+		  return false
+    end
+  end
+end
+
+class RMessage
+  include DataMapper::Resource
+  property :id, 	Serial
+  property :subject, String, default: ""
+  property :body, 	Text, default: ""
+  property :created_at, DateTime, default: DateTime.now
+  property :star, 	Boolean, default: false
+  property :source, String
+  property :new_message, Boolean, default: true
+end

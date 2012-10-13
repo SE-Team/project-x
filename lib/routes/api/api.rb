281e1f37b34b0717df1824a5041cb2797bf2b71e
jon <jonrose08@gmail.com>
2012-10-13 10:02:25 -0700

    add ajax methods for async updating message star boolean in the user messages page

--- lib/routes/api/api.rb
+++ lib/routes/api/api.rb
@@ -44,50 +44,92 @@ post '/api-key' do
   end
 end
 
-get '/api/:api_key/users.json' do
-  valid_key = ApiKey.first(api_key: params["api_key"])
-  if !valid_key.nil?
-    User.all.to_json(only: [:id, :username, :email])
+post '/api/user/rmessage/star' do
+  msg_id = params[:msg_id]
+  msg = RMessage.first(id: msg_id)
+  if msg
+    ## toggle the star icon
+    msg.star = msg.star ? false : true
   end
+  msg.save
+  return msg.star.to_s
 end
 
-get '/api/:api_key/user/:username.json' do
-  User.first(user_name: params["user_name"]).to_json(only: [:username, :id, :email])
+post '/api/user/smessage/star' do
+  msg_id = params[:msg_id]
+  msg = SMessage.first(id: msg_id)
+  if msg
+    ## toggle the star icon
+    msg.star = msg.star ? false : true
+  end
+  msg.save
+  return msg.star.to_s
 end
 
-post '/api/:api_key/event/?' do
-  valid_key = ApiKey.first(api_key: params[:api_key])
-  if !valid_key.nil?
-    event = Event.new
-    event.title = CGI::unescape params[:title]
-    if event.save
-      event.to_json
-    else
-      "BAD ENTRY"
-    end
-  end
-  redirect '/admin/admin/dashboard'
-end
 
-get '/api/:api_key/create-event$:opts' do
-  valid_key = ApiKey.first(api_key: params[:api_key])
-  if !valid_key.nil?
-    User.all.to_json(only: [:id, :username, :email])
-    opts_str = params[:opts].to_s
-    opts = opts_str.split "&"
-    opts.each_with_index.map{|opt, i|
-      kv = opt.split "="
-    opts[i] = [kv[0], kv[1]]}
-    event = Event.new
-    opts.each do |opt|
-      event[opt[0]] = opt[1]
-      p event[opt[0]]
-    end
-    event.user = User.first
-    if event.save
-      event.to_json
-    else
-      "BAD ENTRY"
-    end
-  end
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# get '/api/:api_key/users' do
+#   valid_key = ApiKey.first(api_key: params["api_key"])
+#   if !valid_key.nil?
+#     User.all.to_json(only: [:id, :username, :email])
+#   end
+# end
+
+# get '/api/:api_key/user/:username.json' do
+#   User.first(user_name: params["user_name"]).to_json(only: [:username, :id, :email])
+# end
+
+# post '/api/:api_key/event/?' do
+#   valid_key = ApiKey.first(api_key: params[:api_key])
+#   if !valid_key.nil?
+#     event = Event.new
+#     event.title = CGI::unescape params[:title]
+#     if event.save
+#       event.to_json
+#     else
+#       "BAD ENTRY"
+#     end
+#   end
+#   redirect '/admin/admin/dashboard'
+# end
+
+# get '/api/:api_key/create-event$:opts' do
+#   valid_key = ApiKey.first(api_key: params[:api_key])
+#   if !valid_key.nil?
+#     User.all.to_json(only: [:id, :username, :email])
+#     opts_str = params[:opts].to_s
+#     opts = opts_str.split "&"
+#     opts.each_with_index.map{|opt, i|
+#       kv = opt.split "="
+#     opts[i] = [kv[0], kv[1]]}
+#     event = Event.new
+#     opts.each do |opt|
+#       event[opt[0]] = opt[1]
+#       p event[opt[0]]
+#     end
+#     event.user = User.first
+#     if event.save
+#       event.to_json
+#     else
+#       "BAD ENTRY"
+#     end
+#   end
+# end

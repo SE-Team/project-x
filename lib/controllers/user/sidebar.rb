6339a7ddeba5865c6cba960efa1264ec5f64ebdc
Patrick Edelman <patrick.the.edelman@gmail.com>
2012-10-13 18:54:41 -0700

    changed the links that are displayed in the sidebar

--- lib/controllers/user/sidebar.rb
+++ lib/controllers/user/sidebar.rb
@@ -1,20 +1,11 @@
 module UserSidebarController
 	def user_sidebar_items(user)
 	  [{href: "/user/#{user.user_name}/dashboard", icon: "icon-home", title: "Stream"},
-	   {href: "/user/#{user.user_name}/messages", icon: "icon-envelope", title: "Messages", badge: {value: "#{user.r_messages.all(new_message: true).count}"}},
-	   {href: "/user/#{user.user_name}/dashboard", icon: "icon-comment", title: "Comments", badge: {value: rand(10)}},
-	   :divider,
-	   {href: "/user/#{user.user_name}/friends", icon: "icon-user", title: "Friends"},
+	   {href: "/popular", icon: "icon-fire", title: "Popular Events"},
+	   {href: "/upcoming", icon: "icon-play-circle", title: "Upcoming Events"},
+	   {href: "/newest", icon: "icon-leaf", title: "Newest Events"},
 	   {href: "/user/#{user.user_name}/following", icon: "icon-tag", title: "Following", badge: {value: "#{user.followed_people.count}"}},
-	   {href: "/user/#{user.user_name}/followers", icon: "icon-tags", title: "Followers", badge: {value: "#{user.followers.count}"}},
-	   :divider,
-	   {href: "/user/#{user.user_name}/dashboard", icon: "icon-th", title: "Events", badge: {value: "#{user.events.all(permission: "public").count}"}},
-	   {href: "/user/#{user.user_name}/create-event", icon: "icon-flag", title: "Create Event"},
-	   {href: "/search/", icon: "icon-search", title: "Search"},
-	   {href: "/user/#{user.user_name}/picasa/", icon: "icon-picture", title: "Picasa"},
-	   :divider,
-	   {href: "/user/#{user.user_name}/account", icon:  "icon-pencil", title: "Settings"},
-	   {href: "/logout", icon: "icon-off", title: "Logout"}]
+	   {href: "/user/#{user.user_name}/followers", icon: "icon-tags", title: "Followers", badge: {value: "#{user.followers.count}"}}]
 	end
 
 	def user_sidebar(user)

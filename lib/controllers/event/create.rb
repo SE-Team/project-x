d91073893bc9031458d4ae3b0ac3ab822926ed89
PiculusFlavigula <OrtalisMotmot@gmail.com>
2012-11-22 19:48:42 -0800

    create event form fields modified, autofocus on title, title made required, instr tips using bootsrap popover - but the options dont seem to be being applied,  still need to apply date and time pickers

--- lib/controllers/event/create.rb
+++ lib/controllers/event/create.rb
@@ -1,41 +1,66 @@
 
 
 module CreateEventController
+
+	def create_event_event_info
+	{
+		title: "Event Info",
+		instr: "Provide a title and body (summary/description) for the event.",
+		input_fields:
+		[
+			{name: "Title", type: :text, autofocus: true, required: "required"},
+	    	{name: "Body",  type: :rich_text}
+	    ]
+	}
+	end
+
 	def create_event_time_info
-	  {title: "Time",
-	   input_fields: [{name: "start-date", type: :date, placeholder: "start-date"},
-	                  {name: "start", type: :text, placeholder: "starting (12:00:00)"},
-	                  {name: "end-date", type: :date, placeholder: "end-date"},
-	                  {name: "end", type: :text, placeholder: "ending (12:00:00)"}]}
+	{
+		title: "Time",
+		input_fields:
+		[
+			{name: "Start-Date", type: :date, id: "datepicker"},
+	        {name: "Start-Time", type: :text, id: "timepicker"},
+	        {name: "End-Date",   type: :date, id: "datepicker"},
+	        {name: "End-Time",   type: :text, id: "timepicker"}
+		]
+	}
 	end
 
 	def create_event_location_info
-	  {title: "Location",
-	   input_fields: [{name: "city", type: :text, placeholder: "city"},
-	                  {name: "country", type: :text, placeholder: "country"},
-	                  {name: "country-code", type: :text, placeholder: "country code"}]}
-	end
-
-	def create_event_event_info
-	  {title: "Event Info",
-	   input_fields: [{name: "title", type: :text, placeholder: "title"},
-	                  {name: "body", type: :rich_text, placeholder: "body"}]}
+	{
+		title: "Location",
+	   	input_fields:
+	   	[
+	   		{type: :text}
+	   	]
+	}
 	end
 
 	def create_event_media_info
-	  {title: "Event Media",
-	   input_fields: [{name: "image", type: :text, placeholder: "image"},
-	                  {name: "video", type: :text, placeholder: "video"}]}
+	{
+		title: "Event Media",
+	  	instr: "Specify an absolute URL to embed an image and/or video.",
+	   	input_fields:
+	   	[
+	   		{name: "Image", type: :url, placeholder: "http://www..."},
+	        {name: "Video", type: :url, placeholder: "http://www..."}
+	    ]
+	}
 	end
 
 	def create_event_event_form
-	  {title: "Create an Event",
-	   elements: [create_event_event_info,
-	              create_event_time_info,
-	              create_event_location_info,
-	              create_event_media_info],
-	   action: "/create-event",
-	   method: "post"}
+	{
+		title: "Create an Event",
+	   	elements:
+	   	[
+	   		create_event_event_info,
+	   		create_event_location_info,
+	        create_event_time_info,
+	        create_event_media_info
+	    ],
+	   	action: "/create-event",
+	   	method: "post"}
 	end
 
 end
